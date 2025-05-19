e90f56217a36bfa75af5a1c6670473e65c5735f8
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-05-19 16:38:50 +0200

    Check if the current OS is supported by md-babel
    If the OS is supported, nothing happens.
    
    If the OS is not supported, an error message is shown on the first
    activation of the extension. The error message tells the user that
    currently only Linux and macOS are supported by md-babel.

---
+++ src/os.ts
@@ -0,0 +1,37 @@
+import * as vscode from "vscode";
+import { type } from "node:os";
+
+/**
+ * md-babel runs currently only on macOS and Linux.
+ * @returns whether md-babel runs on the current operating
+ * system
+ */
+export function isOsSupported(): boolean {
+  return ["Linux", "Darwin"].includes(type());
+}
+
+/**
+ * If the extension is activated for the first time and the
+ * OS is not supported by md-babel, that is it is not Linux
+ * or macOS, an error message is shown. To avoid showing the
+ * error message each time, it is stored in the global state
+ * that the extension was already activated.
+ *
+ * Otherwise, nothing happens.
+ *
+ * @param context the context of the extension
+ */
+export async function handleNotSupportedOs(
+  context: vscode.ExtensionContext,
+): Promise<void> {
+  const alreadyActivated = "alreadyActivated";
+
+  if (context.globalState.get<boolean>(alreadyActivated)) {
+    return;
+  }
+
+  vscode.window.showErrorMessage(
+    "md-babel is currently only supported on Linux and macOS.",
+  );
+  await context.globalState.update(alreadyActivated, true);
+}
